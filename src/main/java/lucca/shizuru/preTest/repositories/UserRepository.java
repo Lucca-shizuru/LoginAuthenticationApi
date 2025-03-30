3282582e1a402f97acc4c145ece9c2b80471b97c
Lucca Shizuru <luccashizuru@gmail.com>
2025-03-30 18:22:47 -0300

    Criação Repository, Service e UserRole

---
+++ src/main/java/lucca/shizuru/preTest/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package lucca.shizuru.preTest.repositories;
+
+import lucca.shizuru.preTest.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    UserDetails findByLogin(String login);
+}
