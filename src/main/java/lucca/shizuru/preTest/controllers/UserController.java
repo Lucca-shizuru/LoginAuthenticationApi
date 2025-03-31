aacbd80d898fa53b634bb8338d29e37c6516524e
Lucca Shizuru <luccashizuru@gmail.com>
2025-03-30 21:06:27 -0300

    Criação do Token, configuração e finalização da validação

--- src/main/java/lucca/shizuru/preTest/controllers/UserController.java
+++ src/main/java/lucca/shizuru/preTest/controllers/UserController.java
@@ -2,7 +2,9 @@ package lucca.shizuru.preTest.controllers;
 
 import jakarta.validation.Valid;
 import lucca.shizuru.preTest.dtos.AuthenticationDto;
+import lucca.shizuru.preTest.dtos.LoginResponseDto;
 import lucca.shizuru.preTest.dtos.RegisterDto;
+import lucca.shizuru.preTest.infra.security.TokenService;
 import lucca.shizuru.preTest.models.UserModel;
 import lucca.shizuru.preTest.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +16,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthenticationController {
+public class UserController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDto authenticationDto) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(authenticationDto.userLogin(), authenticationDto.userPassword());
         var auth = this.authenticationManager.authenticate( usernamePassword);
 
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((UserModel) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDto(token));
     }
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDto registerDto) {
